567bcbd4172ac0c38d203cd82f17b8e45e514b96
Ernesto <ernesto@craftxlogic.com>
2024-06-24 16:55:24 +0800

    DS1 updated

--- src/app/d/ds1/Sections/DsoneCopyTwo.tsx
+++ src/app/d/ds1/Sections/DsoneCopyTwo.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function DsoneCopyTwo() {
-  return (
-    <div className='w-full md:w-6/12 mx-auto'>
-      <h1 className='text-3xl md:text-5xl font-bold text-center mb-8 mt-20'>
-        And just remember that the only thing worse than not having something you truly desire, is to have it and then lose it by not taking action at the right time.
-      </h1>
+    return (
+        <div className=" mx-3">
+            <div className=" md:w-6/12 mx-auto w-full  ">
+                <h1 className=" text-2xl text-stone-900 md:text-5xl font-bold text-center mb-8 mt-20">
+                    And just remember that the only thing worse than not having
+                    something you truly desire, is to have it and then lose it
+                    by not taking action at the right time.
+                </h1>
 
-      <p className='text-xl text-center mb-2'>
-        And I do not want to see that happen to you! This is your final opportunity to
-        <span className='font-bold'>save $100! I&apos;m sure you&apos;ll agree that this is an amazing value...</span> Especially when
-        you consider the incredible benefits that ProstaFlow provides with every dose.
-        <span className='font-bold'>Your shipping is 100% free...</span> And this is a one-time-only charge.
-        There are  <span className='font-bold'>NO hidden costs</span> and you are NOT being signed up for a subscription.
-        At this price, we&apos;re actually losing money on the sale... But I&apos;m giving you
-        this opportunity because I truly want you to get the best possible results
-        from ProstaFlow...
-      </p>
-    </div>
-  )
+                <p className="md:text-2xl text-base text-center mx-2 mb-5">
+                    And I do not want to see that happen to you! This is your
+                    final opportunity to
+                    <span className="font-bold">
+                        save $100! I&apos;m sure you&apos;ll agree that this is
+                        an amazing value...
+                    </span>{" "}
+                    Especially when you consider the incredible benefits that
+                    ProstaFlow provides with every dose.
+                    <span className="font-bold">
+                        Your shipping is 100% free...
+                    </span>{" "}
+                    And this is a one-time-only charge. There are{" "}
+                    <span className="font-bold">NO hidden costs</span> and you
+                    are NOT being signed up for a subscription. At this price,
+                    we&apos;re actually losing money on the sale... But I&apos;m
+                    giving you this opportunity because I truly want you to get
+                    the best possible results from ProstaFlow...
+                </p>
+            </div>
+        </div>
+    );
 }
