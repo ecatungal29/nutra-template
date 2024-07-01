4f74e2864d006fbd390c67e84ed292e7a7ec6905
Ernesto <ernesto@craftxlogic.com>
2024-07-01 23:05:49 +0800

    add1.0

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Inter, Poppins } from "next/font/google";
-import "./globals.css";
+import "../src/app/global.css";
 export default function Document() {
     return (
         <Html>
